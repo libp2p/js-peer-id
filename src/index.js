4afbedb525f499f14a771f841200aa6bae687f3a
David Dias <daviddias.p@gmail.com>
2016-05-23 14:16:30 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /*
  * Id is an object representation of a peer Id. a peer Id is a multihash
  */
+
 'use strict'
 
 const fs = require('fs')
@@ -10,8 +11,7 @@ const forge = require('node-forge')
 const protobuf = require('protocol-buffers')
 const path = require('path')
 
-// protobuf read from file
-const messages = protobuf(fs.readFileSync(path.resolve(__dirname, '../protos/crypto.proto')))
+const pbCrypto = protobuf(fs.readFileSync(path.resolve(__dirname, '../protos/crypto.proto')))
 
 exports = module.exports = Id
 
@@ -52,23 +52,25 @@ function Id (id, privKey, pubKey) {
 }
 
 // unwrap the private key protobuf
-function unmarshal (key) {
-  return messages.PrivateKey.decode(key)
+function keyUnmarshal (key) {
+  return pbCrypto.PrivateKey.decode(key)
 }
 
 // create a public key protobuf to be base64 string stored in config
-function marshal (data, type) {
-  var epb
+function keyMarshal (data, type) {
+  const RSA = 0
+
+  let epb
   if (type === 'Public') {
-    epb = messages.PublicKey.encode({
-      Type: 0,
+    epb = pbCrypto.PublicKey.encode({
+      Type: RSA,
       Data: data
     })
   }
 
   if (type === 'Private') {
-    epb = messages.PrivateKey.encode({
-      Type: 0,
+    epb = pbCrypto.PrivateKey.encode({
+      Type: RSA,
       Data: data
     })
   }
@@ -88,10 +90,10 @@ function formatKey (key, type) {
   const nDerBuf = new Buffer(fDerBuf.getBytes(), 'binary')
 
   // protobuf the new DER bytes to the PublicKey Data: field
-  const marshalKey = marshal(nDerBuf, type)
+  const marsheledKey = keyMarshal(nDerBuf, type)
 
   // encode the protobuf public key to base64 string
-  const b64 = marshalKey.toString('base64')
+  const b64 = marsheledKey.toString('base64')
   return b64
 }
 
@@ -148,7 +150,7 @@ exports.createFromPrivKey = function (privKey) {
   const buf = new Buffer(privKey, 'base64')
 
   // get the private key data from the protobuf
-  const mpk = unmarshal(buf)
+  const mpk = keyUnmarshal(buf)
 
   // create a forge buffer
   const fbuf = forge.util.createBuffer(mpk.Data.toString('binary'))
