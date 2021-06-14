42efbb57f188e2581f6f721b28bfdb6b58b37ae7
Nazar Hussain <nazarhussain@gmail.com>
2021-06-15 00:07:58 +0200

    :art: Update PeerId class use JSDoc ts types

--- src/index.js
+++ src/index.js
@@ -1,19 +1,49 @@
-/*
- * Id is an object representation of a peer Id. a peer Id is a multihash
- */
-
 'use strict'
 
 const mh = require('multihashes')
 const CID = require('cids')
+// TODO: Fix missing type
+// @ts-ignore
 const cryptoKeys = require('libp2p-crypto/src/keys')
+// TODO: Fix missing type
+// @ts-ignore
 const withIs = require('class-is')
 const { PeerIdProto } = require('./proto')
 const uint8ArrayEquals = require('uint8arrays/equals')
 const uint8ArrayFromString = require('uint8arrays/from-string')
 const uint8ArrayToString = require('uint8arrays/to-string')
 
+/**
+ * @typedef {import('libp2p-crypto').PrivateKey} PrivateKey
+ * @typedef {import('libp2p-crypto').PublicKey} PublicKey
+ * @typedef {import('libp2p-crypto').KeyType} KeyType
+ * @typedef {import('cids').CIDVersion} CIDVersion
+ */
+
+/**
+ * PeerId JSON format.
+ * @typedef {Object} PeerIdJSON 
+ * @property {string} id - String representation of PeerId.
+ * @property {string} [pubKey] - Public key.
+ * @property {string} [privKey] - Private key.
+ */
+
+/**
+ * Options for PeerId creation.
+ * @typedef {Object} CreateOptions 
+ * @property {number} [bits] - The number of bits to use.
+ * @property {KeyType} [keyType] - The type of key to use.
+ */
+
 class PeerId {
+  /**
+   * Create PeerId object
+   * @constructor
+   * 
+   * @param {Uint8Array} id 
+   * @param {PrivateKey} [privKey]
+   * @param {PublicKey} [pubKey]
+   */
   constructor (id, privKey, pubKey) {
     if (!(id instanceof Uint8Array)) {
       throw new Error('invalid id provided')
@@ -27,8 +57,16 @@ class PeerId {
     this._idB58String = mh.toB58String(this.id)
     this._privKey = privKey
     this._pubKey = pubKey
+
+    /**
+     * @type {string | undefined}
+     */
+    this._idCIDString
   }
 
+  /**
+   * @type {Uint8Array}
+   */
   get id () {
     return this._id
   }
@@ -37,6 +75,9 @@ class PeerId {
     throw new Error('Id is immutable')
   }
 
+  /**
+   * @type {PrivateKey | undefined}
+   */
   get privKey () {
     return this._privKey
   }
@@ -45,6 +86,9 @@ class PeerId {
     this._privKey = privKey
   }
 
+  /**
+   * @type {PublicKey | undefined}
+   */
   get pubKey () {
     if (this._pubKey) {
       return this._pubKey
@@ -71,21 +115,34 @@ class PeerId {
     this._pubKey = pubKey
   }
 
-  // Return the protobuf version of the public key, matching go ipfs formatting
+  /**
+   * Return the protobuf version of the public key, matching go ipfs formatting
+   * 
+   * @returns {Uint8Array | undefined}
+   */
   marshalPubKey () {
     if (this.pubKey) {
       return cryptoKeys.marshalPublicKey(this.pubKey)
     }
   }
 
-  // Return the protobuf version of the private key, matching go ipfs formatting
+  /**
+   * Return the protobuf version of the private key, matching go ipfs formatting
+   * 
+   * @returns {Uint8Array | undefined}
+   */
   marshalPrivKey () {
     if (this.privKey) {
       return cryptoKeys.marshalPrivateKey(this.privKey)
     }
   }
 
-  // Return the protobuf version of the peer-id
+  /**
+   * Return the protobuf version of the peer-id
+   * 
+   * @param {boolean} [excludePriv] - Whether to exclude the private key information from the output.
+   * @returns {Uint8Array}
+   */
   marshal (excludePriv) {
     return PeerIdProto.encode({
       id: this.toBytes(),
@@ -94,6 +151,11 @@ class PeerId {
     }).finish()
   }
 
+  /**
+   * String representation
+   * 
+   * @returns {string}
+   */
   toPrint () {
     let pid = this.toB58String()
     // All sha256 nodes start with Qm
@@ -109,8 +171,11 @@ class PeerId {
     return '<peer.ID ' + pid.substr(0, maxRunes) + '>'
   }
 
-  // return the jsonified version of the key, matching the formatting
-  // of go-ipfs for its config file
+  /**
+   * The jsonified version of the key, matching the formatting of go-ipfs for its config file
+   * 
+   * @returns {PeerIdJSON}
+   */
   toJSON () {
     return {
       id: this.toB58String(),
@@ -119,21 +184,39 @@ class PeerId {
     }
   }
 
-  // encode/decode functions
+  /**
+   * Encode to hex.
+   * 
+   * @returns {string}
+   */
   toHexString () {
     return mh.toHexString(this.id)
   }
 
+  /**
+   * Return raw id bytes 
+   * 
+   * @returns {Uint8Array}
+   */
   toBytes () {
     return this.id
   }
 
+  /**
+   * Encode to base58 string. 
+   * 
+   * @returns {string}
+   */
   toB58String () {
     return this._idB58String
   }
 
-  // return self-describing String representation
-  // in default format from RFC 0001: https://github.com/libp2p/specs/pull/209
+  /**
+   * Self-describing String representation
+   * in default format from RFC 0001: https://github.com/libp2p/specs/pull/209
+   * 
+   * @returns {string}
+   */
   toString () {
     if (!this._idCIDString) {
       const cid = new CID(1, 'libp2p-key', this.id, 'base32')
@@ -143,7 +226,8 @@ class PeerId {
         enumerable: false
       })
     }
-    return this._idCIDString
+
+    return this._idCIDString ? this._idCIDString : ''
   }
 
   /**
@@ -173,14 +257,17 @@ class PeerId {
     return this.equals(id)
   }
 
-  /*
+  /**
    * Check if this PeerId instance is valid (privKey -> pubKey -> Id)
+   * 
+   * @returns {boolean}
    */
   isValid () {
     // TODO: needs better checking
     return Boolean(this.privKey &&
       this.privKey.public &&
       this.privKey.public.bytes &&
+      this.pubKey &&
       this.pubKey.bytes instanceof Uint8Array &&
         uint8ArrayEquals(this.privKey.public.bytes, this.pubKey.bytes))
   }
@@ -202,172 +289,272 @@ class PeerId {
 
     return false
   }
+
+  /**
+   * Create a new PeerId.
+   * 
+   * @param {CreateOptions} opts - Options
+   * @returns {Promise<PeerId>}
+   */
+  static async create (opts) {
+    opts = opts || {}
+    opts.bits = opts.bits || 2048
+    opts.keyType = opts.keyType || 'RSA'
+
+    const key = await cryptoKeys.generateKeyPair(opts.keyType, opts.bits)
+    return computePeerId(key.public, key)
+  }
+
+  /**
+   * Create PeerId from raw bytes.
+   * 
+   * @param {Uint8Array} buf - The raw bytes.
+   * @returns {PeerId}
+   */
+  static createFromBytes (buf) {
+    return new PeerId(buf)
+  }
+
+  /**
+   * Create PeerId from base58-encoded string.
+   * 
+   * @param {string} str - The base58-encoded string.
+   * @returns {PeerId}
+   */
+  static createFromB58String (str) {
+    return PeerId.createFromCID(str) // B58String is CIDv0
+  }
+
+  /**
+   * Create PeerId from hex string.
+   * 
+   * @param {string} str - The hex string.
+   * @returns {PeerId}
+   */
+  static createFromHexString (str) {
+    return new PeerId(mh.fromHexString(str))
+  }
+
+  /**
+   * Create PeerId from CID.
+   * 
+   * @param {CID | CIDVersion | Uint8Array | string} cid - The CID.
+   * @returns {PeerId}
+   */
+  static createFromCID (cid) {
+    cid = CID.isCID(cid) ? cid : new CID(cid)
+    if (!validMulticodec(cid)) throw new Error('Supplied PeerID CID has invalid multicodec: ' + cid.codec)
+    return new PeerId(cid.multihash)
+  }
+
+  /**
+   * Create PeerId from public key.
+   * 
+   * @param {Uint8Array | string} key - Public key, as Uint8Array or base64-encoded string.
+   * @returns {Promise<PeerId>}
+   */
+  static async createFromPubKey (key) {
+    let buf = key
+
+    if (typeof buf === 'string') {
+      buf = uint8ArrayFromString(String(key), 'base64pad')
+    }
+
+    if (!(buf instanceof Uint8Array)) {
+      throw new Error('Supplied key is neither a base64 string nor a Uint8Array')
+    }
+
+    const pubKey = await cryptoKeys.unmarshalPublicKey(buf)
+    return computePeerId(pubKey)
+  }
+
+  /**
+   * Create PeerId from private key.
+   * 
+   * @param {Uint8Array | string} key - Private key, as Uint8Array or base64-encoded string.
+   * @returns {Promise<PeerId>}
+   */
+  static async createFromPrivKey (key) {
+    if (typeof key === 'string') {
+      key = uint8ArrayFromString(key, 'base64pad')
+    }
+
+    if (!(key instanceof Uint8Array)) {
+      throw new Error('Supplied key is neither a base64 string nor a Uint8Array')
+    }
+
+    const privKey = await cryptoKeys.unmarshalPrivateKey(key)
+    return computePeerId(privKey.public, privKey)
+  }
+
+  /**
+   * Create PeerId from PeerId JSON formatted object.
+   * 
+   * @param {PeerIdJSON} obj 
+   * @returns {Promise<PeerId>}
+   */
+  static async createFromJSON (obj) {
+    const id = mh.fromB58String(obj.id)
+    const rawPrivKey = obj.privKey && uint8ArrayFromString(obj.privKey, 'base64pad')
+    const rawPubKey = obj.pubKey && uint8ArrayFromString(obj.pubKey, 'base64pad')
+    const pub = rawPubKey && await cryptoKeys.unmarshalPublicKey(rawPubKey)
+
+    if (!rawPrivKey) {
+      return new PeerId(id, undefined, pub)
+    }
+
+    const privKey = await cryptoKeys.unmarshalPrivateKey(rawPrivKey)
+    const privDigest = await computeDigest(privKey.public)
+
+    /**
+     * @type {Uint8Array}
+     */
+    let pubDigest = new Uint8Array()
+
+    if (pub) {
+      pubDigest = await computeDigest(pub)
+    }
+
+    if (pub && !uint8ArrayEquals(privDigest, pubDigest)) {
+      throw new Error('Public and private key do not match')
+    }
+
+    if (id && !uint8ArrayEquals(privDigest, id)) {
+      throw new Error('Id and private key do not match')
+    }
+
+    return new PeerId(id, privKey, pub)
+  }
+
+  /**
+   * Create PeerId from Protobuf bytes. 
+   * 
+   * @param {Uint8Array | string} buf - Protobuf bytes, as Uint8Array or hex-encoded string.
+   * @returns {Promise<PeerId>}
+   */
+  static async createFromProtobuf (buf) {
+    if (typeof buf === 'string') {
+      buf = uint8ArrayFromString(buf, 'base16')
+    }
+
+    let { id, privKey: privKeyArray, pubKey: pubKeyArray } = PeerIdProto.decode(buf)
+
+    /**
+     * @type {PrivateKey | false}
+     */
+    const privKey = privKeyArray ? await cryptoKeys.unmarshalPrivateKey(privKeyArray) : false
+
+    /**
+     * @type {PublicKey | false}
+     */
+    const pubKey = pubKeyArray ? await cryptoKeys.unmarshalPublicKey(pubKeyArray) : false
+
+    /**
+     * @type {Uint8Array}
+     */
+    let pubDigest = new Uint8Array()
+
+    /**
+     * @type {Uint8Array}
+     */
+    let privDigest = new Uint8Array()
+
+    if (privKey) {
+      // @ts-ignore
+      privDigest = await computeDigest(privKey.public)
+    }
+
+    if (pubKey) {
+      pubDigest = await computeDigest(pubKey)
+    }
+
+    if (privKey) {
+      if (pubKey) {
+        if (!uint8ArrayEquals(privDigest, pubDigest)) {
+          throw new Error('Public and private key do not match')
+        }
+      }
+      // @ts-ignore
+      return new PeerId(privDigest, privKey, privKey.public)
+    }
+
+    // TODO: val id and pubDigest
+    if (pubKey) {
+      return new PeerId(pubDigest, undefined, pubKey)
+    }
+
+    if (id) {
+      return new PeerId(id)
+    }
+
+    throw new Error('Protobuf did not contain any usable key material')
+  }
 }
 
-const PeerIdWithIs = withIs(PeerId, {
+/**
+ * @type {typeof PeerId & {isPeerId: (id: any)=> boolean}}
+ */
+module.exports = withIs(PeerId, {
   className: 'PeerId',
   symbolName: '@libp2p/js-peer-id/PeerId'
 })
 
-exports = module.exports = PeerIdWithIs
+// TODO: withIs is creating "isPeerId" so not sure if its really needed any further.
+/**
+ * Checks if a value is an instance of PeerId.
+ * 
+ * @param {any} peerId - The value to check.
+ * @returns {boolean}
+ */
+module.exports.isPeerId = (peerId) => {
+  return Boolean(typeof peerId === 'object' && peerId._id && peerId._idB58String)
+} 
 
-const computeDigest = (pubKey) => {
+/**
+ * Compute digest.
+ * 
+ * @param {PublicKey} pubKey 
+ * @returns {Promise<Uint8Array>}
+ */
+const computeDigest = async (pubKey) => {
   if (pubKey.bytes.length <= 42) {
-    return mh.encode(pubKey.bytes, 'identity')
+    return Promise.resolve(mh.encode(pubKey.bytes, 'identity'))
   } else {
     return pubKey.hash()
   }
 }
 
-const computePeerId = async (privKey, pubKey) => {
+/**
+ * Compute PeerId.
+ * 
+ * @param {PublicKey} pubKey 
+ * @param {PrivateKey} [privKey]
+ * @returns {Promise<PeerId>}
+ */
+const computePeerId = async (pubKey, privKey) => {
   const digest = await computeDigest(pubKey)
-  return new PeerIdWithIs(digest, privKey, pubKey)
+  return new PeerId(digest, privKey, pubKey)
 }
 
-// generation
-exports.create = async (opts) => {
-  opts = opts || {}
-  opts.bits = opts.bits || 2048
-  opts.keyType = opts.keyType || 'RSA'
-
-  const key = await cryptoKeys.generateKeyPair(opts.keyType, opts.bits)
-  return computePeerId(key, key.public)
-}
-
-exports.createFromHexString = (str) => {
-  return new PeerIdWithIs(mh.fromHexString(str))
-}
-
-exports.createFromBytes = (buf) => {
-  return new PeerIdWithIs(buf)
-}
-
-exports.createFromB58String = (str) => {
-  return exports.createFromCID(str) // B58String is CIDv0
-}
-
+/**
+ * Create PeerId from CID.
+ * 
+ * @param {CID | {codec: string}} cid - The CID.
+ * @returns {boolean}
+ */
 const validMulticodec = (cid) => {
   // supported: 'libp2p-key' (CIDv1) and 'dag-pb' (CIDv0 converted to CIDv1)
   return cid.codec === 'libp2p-key' || cid.codec === 'dag-pb'
 }
 
-exports.createFromCID = (cid) => {
-  cid = CID.isCID(cid) ? cid : new CID(cid)
-  if (!validMulticodec(cid)) throw new Error('Supplied PeerID CID has invalid multicodec: ' + cid.codec)
-  return new PeerIdWithIs(cid.multihash)
-}
-
-// Public Key input will be a Uint8Array
-exports.createFromPubKey = async (key) => {
-  let buf = key
-
-  if (typeof buf === 'string') {
-    buf = uint8ArrayFromString(key, 'base64pad')
-  }
-
-  if (!(buf instanceof Uint8Array)) {
-    throw new Error('Supplied key is neither a base64 string nor a Uint8Array')
-  }
-
-  const pubKey = await cryptoKeys.unmarshalPublicKey(buf)
-  return computePeerId(null, pubKey)
-}
-
-// Private key input will be a string
-exports.createFromPrivKey = async (key) => {
-  if (typeof key === 'string') {
-    key = uint8ArrayFromString(key, 'base64pad')
-  }
-
-  if (!(key instanceof Uint8Array)) {
-    throw new Error('Supplied key is neither a base64 string nor a Uint8Array')
-  }
-
-  const privKey = await cryptoKeys.unmarshalPrivateKey(key)
-  return computePeerId(privKey, privKey.public)
-}
-
-exports.createFromJSON = async (obj) => {
-  const id = mh.fromB58String(obj.id)
-  const rawPrivKey = obj.privKey && uint8ArrayFromString(obj.privKey, 'base64pad')
-  const rawPubKey = obj.pubKey && uint8ArrayFromString(obj.pubKey, 'base64pad')
-  const pub = rawPubKey && await cryptoKeys.unmarshalPublicKey(rawPubKey)
-
-  if (!rawPrivKey) {
-    return new PeerIdWithIs(id, null, pub)
-  }
-
-  const privKey = await cryptoKeys.unmarshalPrivateKey(rawPrivKey)
-  const privDigest = await computeDigest(privKey.public)
-
-  let pubDigest
-
-  if (pub) {
-    pubDigest = await computeDigest(pub)
-  }
-
-  if (pub && !uint8ArrayEquals(privDigest, pubDigest)) {
-    throw new Error('Public and private key do not match')
-  }
-
-  if (id && !uint8ArrayEquals(privDigest, id)) {
-    throw new Error('Id and private key do not match')
-  }
-
-  return new PeerIdWithIs(id, privKey, pub)
-}
-
-exports.createFromProtobuf = async (buf) => {
-  if (typeof buf === 'string') {
-    buf = uint8ArrayFromString(buf, 'base16')
-  }
-
-  let { id, privKey, pubKey } = PeerIdProto.decode(buf)
-
-  privKey = privKey ? await cryptoKeys.unmarshalPrivateKey(privKey) : false
-  pubKey = pubKey ? await cryptoKeys.unmarshalPublicKey(pubKey) : false
-
-  let pubDigest
-  let privDigest
-
-  if (privKey) {
-    privDigest = await computeDigest(privKey.public)
-  }
-
-  if (pubKey) {
-    pubDigest = await computeDigest(pubKey)
-  }
-
-  if (privKey) {
-    if (pubKey) {
-      if (!uint8ArrayEquals(privDigest, pubDigest)) {
-        throw new Error('Public and private key do not match')
-      }
-    }
-    return new PeerIdWithIs(privDigest, privKey, privKey.public)
-  }
-
-  // TODO: val id and pubDigest
-
-  if (pubKey) {
-    return new PeerIdWithIs(pubDigest, null, pubKey)
-  }
-
-  if (id) {
-    return new PeerIdWithIs(id)
-  }
-
-  throw new Error('Protobuf did not contain any usable key material')
-}
-
-exports.isPeerId = (peerId) => {
-  return Boolean(typeof peerId === 'object' &&
-    peerId._id &&
-    peerId._idB58String)
-}
-
-function toB64Opt (val) {
+/**
+ * @param {Uint8Array | undefined} val 
+ * @returns {string}
+ */
+const toB64Opt = (val) => {
   if (val) {
     return uint8ArrayToString(val, 'base64pad')
   }
+
+  return ''
 }
